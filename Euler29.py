110758a7348534a0038c434f316e7c3316d4b018
Ayman Farhat <ayman.farhat1@gmail.com>
2013-08-15 22:41:59 +0300

    Problem 29

---
+++ Euler29.py
@@ -0,0 +1,9 @@
+range = (2, 100)
+terms = {}
+for a in xrange(range[0], range[1]+1):
+	for b in xrange(range[0], range[1]+1):
+		val = a**b
+		if val not in terms:
+			terms[val] = True
+
+print len(terms.keys())
